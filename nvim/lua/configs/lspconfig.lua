fd00035c63b9a288b9e9780c08fd68ea0e77897d
Fabrice <schaubfabrice@gmail.com>
2025-05-07 21:31:51 +0200

    lua config

--- nvim/lua/configs/lspconfig.lua
+++ nvim/lua/configs/lspconfig.lua
@@ -3,4 +3,20 @@ require("nvchad.configs.lspconfig").defaults()
 local servers = { "html", "cssls", "clangd" }
 vim.lsp.enable(servers)
 
+local group_id = vim.api.nvim_create_augroup("UserLspAttach", { clear = true })
+local map = vim.keymap.set
+local lsp = vim.lsp
+
+vim.api.nvim_create_autocmd("LspAttach", {
+  group = group_id,
+  callback = function(ev)
+    local bufnr = ev.buf
+
+    local opts = { buffer = bufnr, remap = false}
+    map("n", "gd", lsp.buf.definition, opts)
+    map("n", "K", lsp.buf.hover, opts)
+    map("n", "<leader>f", function() lsp.buf.format({async = true}) end, opts)
+  end,
+})
+
 -- read :h vim.lsp.config for changing options of lsp servers 
--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -3,8 +3,11 @@ require "nvchad.mappings"
 -- add yours here
 
 local map = vim.keymap.set
+local opts = { noremap = true, silent = true }
 
-map("n", ";", ":", { desc = "CMD enter command mode" })
-map("i", "jk", "<ESC>")
+map("n", ";", ":", {})
+map("i", "jk", "<ESC>", opts)
+map("n", "<A-Up>", "<Cmd>m .-2<CR>==", opts)
+map("n", "<A-Down>", "<Cmd>m .+1<CR>==", opts)
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")

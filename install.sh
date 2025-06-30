04442571e23ad7d38f6d73edf33176040e6c0bb5
Fabrice <fabrice@schaub-dev.xyz>
2025-06-30 20:10:46 +0200

    adds a couple of new dotfiles

--- install.sh
+++ install.sh
@@ -1,3 +1,7 @@
-#ln -sn $(pwd)/nvim $HOME/.config/nvim
-#ln -sn $(pwd)/tmux/tmux.conf $HOME/.tmux.conf
-ln -sfn $(pwd)/configuration.nix  /etc/nixos/configuration.nix
+ln -sfn $(pwd)/nvim $HOME/.config/nvim
+ln -sfn $(pwd)/tmux/tmux.conf $HOME/.tmux.conf
+ln -sfn $(pwd)/sway $HOME/.config/sway 
+ln -sfn $(pwd)/waybar $HOME/.config/waybar 
+ln -sfn $(pwd)/wofi $HOME/.config/wofi
+ln -sfn $(pwd)/alacritty $HOME/.config/alacritty
+# ln -sfn $(pwd)/configuration.nix  /etc/nixos/configuration.nix
